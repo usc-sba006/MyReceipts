48e472595f9914094adf12b0879be35829d09c82
SBA006 <sba006@student.usc.edu.au>
2018-10-01 16:03:40 +1000

    Week 4 work completed - added and designed the first UI fragment (CrimeFragment) and called it in the main CrimeActivity, as well as the necessary Crime class and all other xml updates.

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/Crime.java
@@ -0,0 +1,41 @@
+package com.bignerdranch.android.criminalintent;
+
+import java.util.Date;
+import java.util.UUID;
+
+public class Crime {
+    private UUID mId;
+    private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
+
+    public Crime() {
+
+        mId = UUID.randomUUID();
+        mDate = new Date();
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String mTitle) {
+        this.mTitle = mTitle;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date mDate) {
+        this.mDate = mDate;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean mSolved) {
+        this.mSolved = mSolved;
+    }
+}
